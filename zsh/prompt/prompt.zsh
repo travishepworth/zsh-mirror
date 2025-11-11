d32408810b87acf7f920243269a7575611e66b14
travmonkey <t.coding@batk.me>
2025-11-11 11:26:38 -0700

    Decent state

--- zsh/prompt/prompt.zsh
+++ zsh/prompt/prompt.zsh
@@ -1,19 +1,15 @@
 #!/usr/bin/env zsh
 # prompt.zsh - Pure prompt with Arch updates prepended and Kube context injected
-
 # Source required modules
 source "${0:A:h}/arch-updates.zsh"
 source "${0:A:h}/kube-context.zsh"
-
 # --- PURE PROMPT SETUP ---
 fpath+=($CONFIG_DIR/zsh/pure)
 autoload -U promptinit; promptinit
 prompt pure
 zstyle :prompt:pure:git:stash show yes
-
 # --- KUBERNETES ACTIVATION DETECTION ---
 _KUBE_ACTIVATED=false
-
 detect_kubectl_usage() {
     if [[ "$1" =~ ^(kubectl|kc|oc|kctx)([[:space:]]|$) ]]; then
         _KUBE_ACTIVATED=true
@@ -21,14 +17,10 @@ detect_kubectl_usage() {
         _KUBE_ACTIVATED=false
     fi
 }
-
 # --- HOOKS ---
 autoload -Uz add-zsh-hook
 add-zsh-hook precmd kube_prompt_info
 add-zsh-hook preexec detect_kubectl_usage
-
 # --- CUSTOMIZE PROMPT ---
 setopt prompt_subst
-
-# Prepend arch updates to entire prompt, inject kube before ❯
-PROMPT='$(arch_upd_prompt_segment)'$'\n''${KUBE_PROMPT}'$PROMPT
+PROMPT='$(local seg=$(arch_upd_prompt_segment); [[ -n "$seg" ]] && print "$seg\n")${KUBE_PROMPT}'$PROMPT
